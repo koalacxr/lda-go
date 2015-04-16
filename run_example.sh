4e503822757f331fc65d4a00159e60ea4e7ea497
tengteng <tteng@apple.com>
2015-04-15 18:24:06 -0700

    add example

--- run_example.sh
+++ run_example.sh
@@ -1,1 +1,3 @@
 ./bin/training -accumulate_iterations=10 -burn_in_iterations=50 -compute_loglikelihood=true -corpus_file="testdata/corpus.txt" -model_file="testdata/model.txt" -num_topics=2 -topic_prior=0.1 -word_prior=0.01
+
+./bin/training -accumulate_iterations=10 -burn_in_iterations=50 -compute_loglikelihood=true -corpus_file="testdata/example.txt" -model_file="testdata/example.model.txt" -num_topics=20 -topic_prior=0.1 -word_prior=0.01
